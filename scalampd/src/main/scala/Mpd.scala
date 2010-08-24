8764ed5830b82e08d5ebdf67c2b9df37760132e1
Fred Dubois <fred.dubois@gmail.com>
2010-08-24 07:36:01 -0400

    Playlist parsing works

--- scalampd/src/main/scala/Mpd.scala
+++ scalampd/src/main/scala/Mpd.scala
@@ -6,11 +6,15 @@ import java.util.Calendar
 import javax.xml.bind.DatatypeConverter.parseDateTime
 import scala.io.Source
 
-class ParsingException(msg: String) extends Exception(msg)
+class MpdException(msg: String) extends Exception(msg)
+class MpdParsingException(msg: String) extends MpdException(msg)
+class MpdConnectionException(msg: String) extends MpdException(msg)
 
-case class Playlist private[mpd] (val name: String, val lastModified: String)
+case class Playlist private[mpd] (val name: String, val lastModified: Calendar)
 
 class PlaylistManager private[mpd] (private val mpd: Mpd) {
+
+  import ParseHelpers._
 
   private object Attributes extends Enumeration {
     type Attributes = Value
@@ -18,42 +22,37 @@ class PlaylistManager private[mpd] (private val mpd: Mpd) {
     val LastModified = Value("Last-Modified")
   }
 
-  def testParse = println(parse("playlist: test", Attributes.Playlist, unit))
-  def testParse2 = println(parse("Last-Modified: 2010-06-27T20:23:18Z", Attributes.LastModified, parseDateTime))
+  def list = parsePlaylists(mpd sendCommand Command.ListPlaylists)
 
-  def list = mpd.sendCommand(Command.ListPlaylists)
+  def parsePlaylists(lines: List[String]): List[Playlist] = {
+    lines match {
+      case name :: date :: rest => {
+        val parsedName = parse(name, Attributes.Playlist)
+        val parsedDate = parse(date, Attributes.LastModified, parseDateTime)
+        Playlist(parsedName, parsedDate) :: parsePlaylists(rest)
+      }
+      case "OK" :: Nil => Nil
+      case data => throw new MpdParsingException("Unexpected value '" + data + "'")
+    }
+  }
+}
+
+private[mpd] object ParseHelpers {
+
+  def parse[E >: Enumeration](line: String, prefix: E): String = parse(line, prefix, unit)
 
   def parse[T, E >: Enumeration](line: String, prefix: E, convert: String => T): T = {
     val elems = line.split(": ").toList
     if (elems.length != 2) {
-      throw new ParsingException("Result of split not 2 elements")
+      throw new MpdParsingException("Result of split not 2 elements")
     }
     if (prefix.toString != elems(0)) {
-      throw new ParsingException("Prefix " + prefix + " doesn't match parsed prefix " + elems(0))
+      throw new MpdParsingException("Prefix " + prefix + " doesn't match parsed prefix " + elems(0))
     }
     convert(elems(1))
   }
 
   def unit[T](x: T): T = x
-
-  def parsePlaylists(reply: List[String]): List[Playlist] = {
-    def _parse(lines: List[String]): List[Playlist] = {
-      lines match {
-        case name :: date :: rest => {
-          val playlistName = parse(name, Attributes.Playlist, unit)
-          Playlist(name, date) :: _parse(rest)
-        }
-        case _ => Nil
-      }
-    }
-    _parse(reply)
-  }
-}
-
-private[mpd] object Helpers {
-  def parse[T](reply: List[String], attributes: List[Enumeration]): List[T] = {
-    null
-  }
 }
 
 private[mpd] object Command extends Enumeration {
@@ -70,7 +69,7 @@ class Mpd(private val _hostname: String, port: Int) {
   private var in: LineNumberReader = _
   private var out: PrintWriter = _
 
-  lazy val playlists = new PlaylistManager(this)
+  lazy val playlistManager = new PlaylistManager(this)
 
   println("Instantiating Mpd")
   println("Host: " + host)
@@ -83,9 +82,8 @@ class Mpd(private val _hostname: String, port: Int) {
     out = new PrintWriter(socket.getOutputStream)
     val reply = readOutput
     if (reply.length != 1 || !reply(0).startsWith("OK")) {
-      throw new Exception("Connection failed: " + reply)
+      throw new MpdConnectionException("Connection failed: " + reply)
     }
-    println("Connected")
   }
 
   protected def reconnect {
@@ -94,7 +92,6 @@ class Mpd(private val _hostname: String, port: Int) {
   }
 
   private[mpd] def sendCommand(command: Command.Value) = {
-    println("Executing command " + command)
     out.println(command)
     out.flush
     readOutput
@@ -104,7 +101,6 @@ class Mpd(private val _hostname: String, port: Int) {
     def read(lines: List[String]): List[String] =
       if (in.ready) {
         val line = in.readLine()
-        println(line)
         if (line == null) lines else read(line :: lines)
       } else {
         lines
@@ -113,21 +109,17 @@ class Mpd(private val _hostname: String, port: Int) {
   }
 
   def close {
-    out.print("close")
     out.close
     in.close
     socket.close
-    println("Mpd connection closed")
   }
 }
 
 object Mpd extends Application {
   val mpd = new Mpd("127.0.0.1", 6600)
-  val playlists = mpd.playlists
-  playlists.testParse
-  playlists.testParse2
+  val playlistManager = mpd.playlistManager
   println("Fetching playlists")
-  val plists = playlists.list
-  println(plists)
+  val playlists = playlistManager.list
+  println(playlists)
   mpd.close
 }
