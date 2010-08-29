657cb2baf528145d3ad26213d3703315137383ad
Fred Dubois <fred.dubois@gmail.com>
2010-08-28 21:35:55 -0400

    Listing playlist songs works
    Also refactored sendCommand to allow optional parameters

--- scalampd/src/main/scala/Mpd.scala
+++ scalampd/src/main/scala/Mpd.scala
@@ -12,47 +12,18 @@ class MpdParsingException(msg: String) extends MpdException(msg)
 class MpdConnectionException(msg: String) extends MpdException(msg)
 class MpdCommunicationException(msg: String) extends MpdException(msg)
 
+/*
+* Utility objects
+*/
 private[mpd] object DateFormatter {
   private val dateFormatter = new SimpleDateFormat("yyyy-MM-dd G 'at' HH:mm:ss z")
   def apply(date: Date) = dateFormatter.format(date)
 }
 
-sealed abstract class MpdObject(lines: List[String])
-
-case class MpdPlaylist private[mpd] (lines: List[String]) extends MpdObject(lines){
-  import javax.xml.bind.DatatypeConverter.parseDateTime
-  val name = lines(0)
-  val lastModified: Calendar = parseDateTime(lines(1))
-}
-
-case class OldMpdPlaylist private[mpd] (val name: String, val lastModified: Calendar) {
-  override def toString = "Playlist name: " + name + ", last modified date: " + DateFormatter(lastModified.getTime)
-}
-
-private[mpd] abstract class MpdObjectAttribute extends Enumeration {
-  protected class AttributeValue(name: String, converter: String => AnyRef) extends Val(name)
-}
-
-private[mpd] object PlaylistAttribute extends MpdObjectAttribute {
-  val Playlist = Value("playlist")
-  val LastModified = Value("Last-Modified")
-}
-
-class PlaylistManager private[mpd] (private val mpd: Mpd) {
-  import ParseHelpers.parse
-  import PlaylistAttribute.{Playlist,LastModified}
-
-  def list = parsePlaylists(mpd sendCommand Command.ListPlaylists)
-
-  def parsePlaylists(lines: List[String]): List[MpdPlaylist] = {
-    parse(lines, List(Playlist, LastModified), MpdPlaylist(_))
-  }
-}
-
 private[mpd] object ParseHelpers {
+  import Function.tupled
 
-  def parse[T <: MpdObject](lines: List[String], mpdAttributes: List[MpdObjectAttribute#Value], create: List[String] => T): List[T] = {
-    import Function.tupled
+  def parse[T <: MpdObject](lines: List[String], create: List[String] => T, mpdAttributes: List[MpdObjectAttribute#Value]): List[T] = {
     def _parse(linesToParse: List[String]): List[T] = {
       if (linesToParse == Nil) {
         Nil
@@ -72,8 +43,54 @@ private[mpd] object ParseHelpers {
     else
       throw new MpdParsingException("Was expecting line to start with '" + expectedAttribute + "' but started with '" + attributeName + "' instead")
   }
+}
 
-  def unit[T](x: T): T = x
+/*
+* MPD objects
+*/
+sealed abstract class MpdObject(rawData: List[String])
+
+case class MpdSong private[mpd] (rawData: List[String]) extends MpdObject(rawData) {
+}
+
+case class MpdPlaylist private[mpd] (mpd: Mpd, private val rawData: List[String]) extends MpdObject(rawData){
+  import javax.xml.bind.DatatypeConverter.parseDateTime
+
+  if (rawData.length != 2)
+    throw new IllegalArgumentException("Was expecting List of 2 strings")
+
+  val name = rawData(0)
+  val lastModified: Calendar = parseDateTime(rawData(1))
+
+  def listSongs = mpd sendCommand(Command.ListPlaylist, name) foreach println
+
+  def listSongsDetailed = mpd sendCommand(Command.ListPlaylistInfo, name) foreach println
+}
+
+/*
+* MPD object attributes
+*/
+private[mpd] abstract class MpdObjectAttribute extends Enumeration {
+  protected class AttributeValue(name: String, converter: String => AnyRef) extends Val(name)
+}
+
+private[mpd] object PlaylistAttribute extends MpdObjectAttribute {
+  val Playlist = Value("playlist")
+  val LastModified = Value("Last-Modified")
+}
+
+/*
+* MPD managers
+*/
+class PlaylistManager private[mpd] (private val mpd: Mpd) {
+  import ParseHelpers.parse
+  import PlaylistAttribute.{Playlist,LastModified}
+
+  def getPlaylists = parsePlaylists(mpd sendCommand Command.ListPlaylists)
+
+  private def parsePlaylists(lines: List[String]): List[MpdPlaylist] = {
+    parse(lines, MpdPlaylist(mpd, _), List(Playlist, LastModified))
+  }
 }
 
 private[mpd] object Command extends Enumeration {
@@ -81,6 +98,8 @@ private[mpd] object Command extends Enumeration {
 
   val Close = Value("close")
   val ListPlaylists = Value("listplaylists")
+  val ListPlaylist = Value("listplaylist")
+  val ListPlaylistInfo = Value("listplaylistinfo")
 }
 
 class Mpd(private val _hostname: String, port: Int) {
@@ -108,9 +127,12 @@ class Mpd(private val _hostname: String, port: Int) {
     connect
   }
 
-  private[mpd] def sendCommand(command: Command.Value) = {
-    val out = socket.getOutputStream
-    out.write((command + "\n").getBytes)
+  private[mpd] def sendCommand(command: Command.Value, options: String*) = {
+    val out = new BufferedWriter(new OutputStreamWriter(socket getOutputStream))
+    out write command.toString
+    options foreach { (opt) => out.write(" " + opt) }
+    out write "\n"
+    out.flush
     readOutput
   }
 
@@ -121,7 +143,7 @@ class Mpd(private val _hostname: String, port: Int) {
         case null => lines
         case line =>
           if (line startsWith "ACK")
-            throw new MpdCommunicationException("Bad command")
+            throw new MpdCommunicationException("Bad command: " + line)
           else if (line startsWith "OK")
             lines
           else
@@ -140,6 +162,9 @@ object Mpd extends Application {
   val mpd = new Mpd("127.0.0.1", 6600)
   val playlistManager = mpd.playlistManager
   println("Saved playlists:")
-  playlistManager.list foreach println
+  val playlist = playlistManager.getPlaylists foreach { playlist =>
+    println(playlist)
+    playlist.listSongsInfo
+  }
   mpd.close
 }
